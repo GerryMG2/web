3592797bee1dc81a052d77c85a2e371a4379925b
GerryMG2 <37318994+GerryMG2@users.noreply.github.com>
2018-12-01 12:37:06 -0600

    firsts

---
+++ routes.js
@@ -0,0 +1,92 @@
+'use strict'
+
+const Joi = require('joi')
+const site = require('./controllers/site')
+const user = require('./controllers/user')
+const question = require('./controllers/question')
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: site.home
+  },
+  {
+    method: 'GET',
+    path: '/register',
+    handler: site.register
+  },
+  {
+    path: '/create-user',
+    method: 'POST',
+    options: {
+      validate: {
+        payload: {
+          name: Joi.string().required().min(3),
+          email: Joi.string().email().required(),
+          password: Joi.string().required().min(6)
+        },
+        failAction: user.failValidation
+      }
+    },
+    handler: user.createUser
+  },
+  {
+    method: 'GET',
+    path: '/login',
+    handler: site.login
+  },
+  {
+    method: 'GET',
+    path: '/logout',
+    handler: user.logout
+  },
+  {
+    method: 'GET',
+    path: '/ask',
+    handler: site.ask
+  },
+  {
+    path: '/validate-user',
+    method: 'POST',
+    options: {
+      validate: {
+        payload: {
+          email: Joi.string().email().required(),
+          password: Joi.string().required().min(6)
+        },
+        failAction: user.failValidation
+      }
+    },
+    handler: user.validateUser
+  },
+  {
+    path: '/create-question',
+    method: 'POST',
+    options: {
+      validate: {
+        payload: {
+          title: Joi.string().required(),
+          description: Joi.string().required()
+        },
+        failAction: user.failValidation
+      }
+    },
+    handler: question.createQuestion
+  },
+  {
+    method: 'GET',
+    path: '/assets/{param*}',
+    handler: {
+      directory: {
+        path: '.',
+        index: ['index.html']
+      }
+    }
+  },
+  {
+    method: ['GET', 'POST'],
+    path: '/{any*}',
+    handler: site.notFound
+  }
+]
